140fad249dca48df356f99ef35634f12912b72b1
Jaime-Jesus-Zamora-Ramirez <84102510+Jaime-Jesus-Zamora-Ramirez@users.noreply.github.com>
2023-03-29 21:22:33 -0600

    Delete CodigoFuente directory

--- CodigoFuente/lib/rutas/screens-colores.dart
+++
@@ -1,6 +0,0 @@
-//screen de la pantalla de practica de colores
-export  'package:pruebasnav/Practica_Colores/practica_rojo.dart'; //Se exportan las pantallas para la ruta del color Rojo
-export  'package:pruebasnav/Practica_Colores/practica_rosa.dart'; //Se exportan las pantallas para la ruta del color Rosa
-export  'package:pruebasnav/Practica_Colores/practica_salmon.dart'; //Se exportan las pantallas para la ruta del color Salmon
-export  'package:pruebasnav/Practica_Colores/practica_verde.dart'; //Se exportan las pantallas para la ruta del color Verde
-export  'package:pruebasnav/Practica_Colores/practica_azul.dart'; //Se exportan las pantallas para la ruta del color Azul
--- CodigoFuente/lib/rutas/screens-menu.dart
+++
@@ -1,2 +0,0 @@
-//screen de la pantalla del Menu Principal
-export  'package:pruebasnav/menu_principal/slider_menu.dart';  //Se exportan las pantallas para la ruta del Menu Principal
